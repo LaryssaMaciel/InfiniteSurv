08544f13e649b5d692e915f87c632ad58f7b3a42
LaryssaMaciel <maciel.f.laryssa@gmail.com>
2022-05-08 14:56:51 -0300

    fixed second attack
    ajuste no tiro e troca do botão de ataque por joystick

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,36 +5,43 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    private float xMin, xMax, yMin, yMax;
-    public SpriteRenderer mapa;
+    [Header("Background")]
+    private float xMin, xMax, yMin, yMax; // bordas pra limitar câmera de sair do mapa
+    public SpriteRenderer mapa; // background
 
-    public FixedJoystick fj, fjatk;
+    [Header("Joysticks")]
+    public FixedJoystick fj, fjatk; // joysticks -> Mov | Ataque
 
-    private SceneController sm;
-
+    [Header("Shoot")]
     public GameObject pTiro, posTiro;
     public string tipoAtaque = "axe";
+    private bool atk2down = false;
 
+    [Header("Movimentação")]
     public Rigidbody2D rb;
     private float movSpeed = 5, moveforce = 5f;
     public Vector2 mov;
     private bool viraDir = true;
 
+    [Header("Vida")]
     public Image hpBar;
     public float vida, fullvida = 100, cura = 10;
 
+    [Header("Cura")]
     public int vidasExtra = 0;
     private GameObject vidasBtn;
     public Text curasNum;
 
+    [Header("Ataque/Dano")]
     public bool canDano = true; // se pode tomar ataque
-
     public bool enemyInRange = false, canAttack = true, atacando = false;
     public float ataqueValor = 10, timeBtwAttack, startTimeBtwAttack = .3f, attackRange;
     public Transform attackPos;
     public LayerMask enemiesLayer;
 
+    [Header("Others")]
     public ChangeManager cm;
+    private SceneController sm;
 
     void Awake()
     {
@@ -47,23 +54,15 @@ public class Player : MonoBehaviour
         fjatk = GameObject.FindGameObjectWithTag("fjatk").GetComponent<FixedJoystick>();
         cm = GameObject.Find("Armas").GetComponent<ChangeManager>();
         vidasBtn = GameObject.Find("Curas");
-
-        var spriteSize = GetComponent<SpriteRenderer>().bounds.size.y * .5f; // Working with a simple box here, adapt to you necessity
- 
-        var camHeight = mapa.bounds.size.y/2;
-        var camWidth = mapa.bounds.size.x/2;
-
-        yMin = -camHeight + spriteSize; // lower bound
-        yMax = camHeight - spriteSize; // upper bound
-        
-        xMin = -camWidth + spriteSize; // left bound
-        xMax = camWidth - spriteSize; // right bound 
+        LimitCamera();
     }
  
     void Update()
     {
         if (timeBtwAttack <= 0) { timeBtwAttack = 0; }
         else { timeBtwAttack -= Time.deltaTime; }
+
+        Ataque2();
 
         if (vida <= 0) { vida = 0; sm.ChangeScene("GameOver"); }
         else if (vida > fullvida) { vida = fullvida; }
@@ -84,6 +83,20 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         Movimentacao(); 
+    }
+
+    void LimitCamera()
+    {
+        var spriteSize = GetComponent<SpriteRenderer>().bounds.size.y * .5f; // Working with a simple box here, adapt to you necessity
+        
+        var camHeight = mapa.bounds.size.y/2;
+        var camWidth = mapa.bounds.size.x/2;
+
+        yMin = -camHeight + spriteSize; // lower bound
+        yMax = camHeight - spriteSize; // upper bound
+        
+        xMin = -camWidth + spriteSize; // left bound
+        xMax = camWidth - spriteSize; // right bound 
     }
 
     void OnTriggerStay2D(Collider2D col)
@@ -129,29 +142,23 @@ public class Player : MonoBehaviour
         if (timeBtwAttack <= 0)
         {
             canAttack = true;
-            if (canAttack)
+            if (tipoAtaque == "axe")
             {
-                if (tipoAtaque == "axe")
-                {
-                    ataqueValor = 10;
-                    Ataque1();
-                    startTimeBtwAttack = .3f;
-                }
-                else if (tipoAtaque == "tiro")
-                {
-                    startTimeBtwAttack = 0;
-                    ataqueValor = 5;
-                    Ataque2();
-                    
-                }
-                timeBtwAttack = startTimeBtwAttack;
+                ataqueValor = 10;
+                Ataque1();
+                startTimeBtwAttack = .3f;
             }
+            if (tipoAtaque == "tiro")
+            {
+                ataqueValor = 5;
+                atk2down = true;
+            }
+            timeBtwAttack = startTimeBtwAttack;
         }
-        else 
-        { 
-            canAttack = false;
-        }
+        else { canAttack = false; }
     }
+
+    public void Atk2Up() { atk2down = false; }
 
     public void Ataque1() // ataque curta distancia
     {
@@ -168,13 +175,26 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void Ataque2() // ataque longa distancia
+    public void Tiro() // ataque longa distancia
     {
         GameObject a = Instantiate(pTiro, posTiro.transform.position, posTiro.transform.rotation);
         float angle = Mathf.Atan2(fjatk.Horizontal, fjatk.Vertical) * Mathf.Rad2Deg;
         posTiro.transform.eulerAngles = new Vector3(0, 0, -angle);
         a.GetComponent<Rigidbody2D>().AddForce(posTiro.transform.up * 20, ForceMode2D.Impulse);
         StartCoroutine(WaitBullet(a));
+    }
+
+    private void Ataque2()
+    {
+        if (atk2down && canAttack)
+        {
+            if (timeBtwAttack <= 0)
+            {
+                startTimeBtwAttack = .2f;
+                timeBtwAttack = startTimeBtwAttack;
+                Tiro();
+            }
+        }
     }
 
     IEnumerator WaitBullet(GameObject a) { yield return new WaitForSeconds(1f); Destroy(a); }
