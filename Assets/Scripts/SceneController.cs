3a582c582dbb17753e2ea887e163c9d02f2975b3
LaryssaMaciel <maciel.f.laryssa@gmail.com>
2022-05-28 21:30:50 -0300

    adicionei umas coisas pra audio
    só setei os mixers, nao foram testados

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
@@ -9,10 +11,6 @@ public class SceneController : MonoBehaviour
     void Awake() { sc = this; }
 
     public Animator animFade;
-
-    void Start()
-    {
-    }
 
     public void ChangeScene(string nome)
     {
@@ -84,5 +82,63 @@ public class SceneController : MonoBehaviour
     {   
         credits.SetActive(false);
         menu.SetActive(true);
+    }
+
+    [Header("Todos os sons")]
+    public AudioClip[] som; // sons
+    public AudioClip[] musicasCena;
+    public AudioSource audioSource; // audio
+
+    [Header("Mixers")]
+    public AudioMixer MusMixer;
+    public AudioMixer GamMixer;
+
+    [Header("Sliders config. audio")]
+    public Slider sliMus, sliGam;
+
+    private float musVol = 1, gameVol = 1; // volumes
+    private string sceneName; // nome da cena
+
+    void Start()
+    {   // configura variaveis
+        audioSource = GetComponent<AudioSource>();
+        sceneName = SceneManager.GetActiveScene().name.ToString();
+        // switch (sceneName)
+        // {   // define musica principal da cena
+        //     case "Menu":
+        //         AudioManager(0); // som de menu
+        //         break;
+        //     case "Jogo":
+        //         AudioManager(0);
+        //         break;
+        //     case "GameOver":
+        //         AudioManager(0); // som de gameplay
+        //         break;
+        // }
+        // pega valores salvos das configuracoes de audio
+        musVol = PlayerPrefs.GetFloat("musicaVol");
+        SetMusVolume(musVol);
+        if (sliMus != null) { sliMus.value = musVol; }
+        gameVol = PlayerPrefs.GetFloat("gameVol");
+        SetGamVolume(gameVol);
+        if (sliGam != null) {sliGam.value = gameVol; }
+    }
+
+    public void SetMusVolume(float vol) // define e salva configuracoes do volume da musica principal
+    {
+        MusMixer.SetFloat("volume", vol);
+        PlayerPrefs.SetFloat("musicaVol", vol);
+    }
+
+    public void SetGamVolume(float vol) // define e salva configuracoes do volume de audio da gameplay
+    {
+        GamMixer.SetFloat("volume", vol);
+        PlayerPrefs.SetFloat("gameVol", vol);
+    }
+
+    public void AudioManager(int audio)
+    {
+        audioSource.clip = musicasCena[audio];
+        audioSource.Play();
     }
 }
